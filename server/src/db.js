b52d53a26c89a33ac9bd2d6a4ba8fb33df8cabc5
David Peñavasquez <davidpv96@hotmail.com>
2023-10-31 13:13:06 -0500

    base de datos actualizada

--- server/src/db.js
+++ server/src/db.js
@@ -7,7 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/drivers`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/driversdb`, {
   logging: false, 
   native: false, 
 });
